2927da89654e1f4557db22e919adb8b1f27fedc8
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-21 12:52:15 +0100

    chore: add missing env variable error

--- initializers/loadEnvironmentVariables.go
+++ initializers/loadEnvironmentVariables.go
@@ -17,7 +17,13 @@ func LoadEnvironmentVariables() {
 	}
 
 	GMAIL_APP_PASSWORD = os.Getenv("GMAIL_APP_PASSWORD")
+	if GMAIL_APP_PASSWORD == "" {
+		log.Fatal("GMAIL_APP_PASSWORD environment variable not set")
+	}
 	GMAIL_USERNAME = os.Getenv("GMAIL_USERNAME")
+	if GMAIL_USERNAME == "" {
+		log.Fatal("GMAIL_USERNAME environment variable not set")
+	}
 
 	log.Println("Successfully loaded environment variables")
 }
